7c6652e92b763513d272cd2d9a75970b5071ea87
Lyubomir Iliev <iliev.lyubomir98@gmail.com>
2024-11-13 19:47:52 +0200

    videos edit

--- components/Videos.tsx
+++ components/Videos.tsx
@@ -3,16 +3,16 @@ import { videosData } from "@/utils/constants";
 
 const Videos: React.FC = () => {
   return (
-    <section className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center items-center max-w-screen-lg gap-4 px-2 py-8">
+    <section className="w-full grid grid-cols-2 md:grid-cols-3 lg:flex justify-center items-center max-w-screen-lg gap-4 lg:gap-0 py-8">
       {videosData.map((video, index) => (
         <div
           key={index}
-          className="bg-cardBG rounded-2xl p-2 flex w-full h-[400px] items-center justify-center"
+          className="bg-cardBG rounded-2xl flex w-full h-[400px] items-center justify-center"
         >
           <video
             className="w-full h-full object-fit rounded-2xl"
-            controls // Allows user control (play, pause, volume, etc.)
-            src={video} // URL for video source
+            controls
+            src={video}
             preload="metadata" // Preloads metadata for faster loading
           >
             Your browser does not support the video tag.
